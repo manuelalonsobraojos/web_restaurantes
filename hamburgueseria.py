6935d74889838eaae5744f09eec92812b303bd10
manuelalonsobraojos <manuelalonso136@gmail.com>
2017-01-03 14:15:30 +0100

    Actualizando archivo

--- hamburgueseria.py
+++ hamburgueseria.py
@@ -113,5 +113,7 @@ def page_not_found(e):
 if __name__ == "__main__":
 	#app.debug = false
 	#app.run(host='0.0.0.0')
-	port=int(os.environ.get('PORT',5000))
-	app.run(host='0.0.0.0', port=port)
+	#port=int(os.environ.get('PORT',5000))
+	#app.run(host='0.0.0.0', port=port)
+
+    	app.run(host='0.0.0.0', port='80')
