997c63a7d0ebd913cbfd5972bfeab38d4c11ec54
manuelalonsobraojos <manuelalonso136@gmail.com>
2017-01-03 13:03:43 +0100

    Actualizando archivo

--- hamburgueseria.py
+++ hamburgueseria.py
@@ -111,5 +111,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 	
 if __name__ == "__main__":
-	app.debug = True
-	app.run(host='0.0.0.0')
+	app.debug = False
+	#app.run(host='0.0.0.0')
+	port=int(os.environ.get('PORT',5000))
+	app.run(host='0.0.0.0', port=port)
